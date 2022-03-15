e3d613a10dd61d1b89b97f54289078a9e0118d87
cristiani <cristian1997ion@yahoo.com>
2022-03-15 10:37:57 +0200

    use application.properties for db config, adds example

--- src/main/java/com/terralink/terralink_api/TerralinkApiApplication.java
+++ src/main/java/com/terralink/terralink_api/TerralinkApiApplication.java
@@ -1,18 +1,21 @@
 package com.terralink.terralink_api;
 
-import java.io.InputStream;
 import java.util.Properties;
 
 import javax.persistence.Persistence;
 
 import org.hibernate.reactive.mutiny.Mutiny;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class TerralinkApiApplication {
+
+    @Autowired
+    private Environment env;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TerralinkApiApplication.class, args);
@@ -20,12 +23,11 @@ public class TerralinkApiApplication {
 
     @Bean
     public Mutiny.SessionFactory sessionFactory() {
+        
         Properties props = new Properties();
-        try (InputStream stream = new ClassPathResource("env.properties").getInputStream()) {
-            props.load(stream);
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
+        props.setProperty("javax.persistence.jdbc.url", this.env.getProperty("javax.persistence.jdbc.url"));
+        props.setProperty("javax.persistence.jdbc.user", this.env.getProperty("javax.persistence.jdbc.user"));
+        props.setProperty("javax.persistence.jdbc.password", this.env.getProperty("javax.persistence.jdbc.password"));
 
         return Persistence
             .createEntityManagerFactory("terralinkPU", props)
